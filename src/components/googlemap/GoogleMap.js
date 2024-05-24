67dbb6189ee7ca17f9e6729d054b7cdf165ddd78
Kenneth Cabobos <kennethdcabobos@iskolarngbayan.pup.edu.ph>
2024-05-24 10:38:37 +0800

    test gcloud backend

--- src/components/googlemap/GoogleMap.js
+++ src/components/googlemap/GoogleMap.js
@@ -26,7 +26,7 @@ import io from "socket.io-client"
 const Map = (props) => {
 
   // Declarations
-  const socket = io.connect("https://tq03tq0w-3001.asse.devtunnels.ms")
+  const socket = io.connect("https://kyusitrip-backend-v65tufifbq-de.a.run.app")
 
   // ------------------------------------------------------------ //
   // Props
